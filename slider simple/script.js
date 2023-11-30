c63e9357b1d300d13190f368403c167680913dd1
Alexey <arya065@g.educaand.es>
2023-11-30 21:13:13 +0100

    ini

--- slider simple/script.js
+++ slider simple/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     let offset = 0; // смещение от левого края
     const sliderLine = document.querySelector(".slider-line");
+    //go to the right
     document.querySelector(".slider-next").addEventListener('click', function () {
         offset += 380;
         if (offset > 1140) {
@@ -8,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         sliderLine.style.left = -offset + "px";
     });
+    //go to the left
     document.querySelector(".slider-prev").addEventListener('click', function () {
         offset -= 380;
         if (offset < 0) {
