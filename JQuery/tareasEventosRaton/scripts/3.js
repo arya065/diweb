4fea469db0130e84a7eef152c629c4ec3c9f6068
arya065 <arya065@g.educaand.es>
2024-02-12 23:13:55 +0100

    ini

--- JQuery/tareasEventosRaton/scripts/3.js
+++ JQuery/tareasEventosRaton/scripts/3.js
@@ -1,2 +1,10 @@
 $(document).ready(function () {
+    $("#menu").children("li").children("a").hover(function () {
+        $(this)
+            .css("color", "#5d2e2e")
+    });
+    $("#menu").children("li").children("a").mouseleave(function () {
+        $(this)
+            .css("color", "#FFF")
+    });
 });
