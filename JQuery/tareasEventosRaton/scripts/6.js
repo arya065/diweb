b5d8c4a9cb1950555fe18f0c1fe40b349520e1dd
arya065 <arya065@g.educaand.es>
2024-02-13 13:58:02 +0100

    ini

---
+++ JQuery/tareasEventosRaton/scripts/6.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    // $("body").css({ "position": "relative" ,"width": "100%" });
+    $("header")
+    .css({ "position": "sticky", "top": "0", "width": "100%", "z-index": "5", "background-color": "white" });
+});
