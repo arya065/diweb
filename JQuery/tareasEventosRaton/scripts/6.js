cd6976fd3c8133b8a1d52482282cf3fef3194b2b
arya065 <arya065@g.educaand.es>
2024-02-16 13:55:04 +0100

    ini

--- JQuery/tareasEventosRaton/scripts/6.js
+++ JQuery/tareasEventosRaton/scripts/6.js
@@ -1,5 +1,17 @@
+function hide() {
+    $("#menu").css({ "display": "none", "flex-direction": "row", "list-style-type": "none", "justify-content": "space-between" });
+    $("#menu>*").css({ "display": "none" });
+    $("#menu-principal").css("height", "50px");
+}
+
 $(document).ready(function () {
-    // $("body").css({ "position": "relative" ,"width": "100%" });
     $("header")
     .css({ "position": "sticky", "top": "0", "width": "100%", "z-index": "5", "background-color": "white" });
+    $("#menu-principal").hover(function () {
+        $("#menu").css({ "display": "flex", "flex-direction": "row", "list-style-type": "none", "justify-content": "space-between" });
+        $("#menu>*").css({ "display": "block" });
+        $("#menu-principal").css("height", "100px");
+    })
+    $("#menu-principal").mouseleave(() => hide())
+    $(window).resize(() => hide())
 });
