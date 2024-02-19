7101d000398b0074fb69058bd7b801a41fd1ce77
arya065 <arya065@g.educaand.es>
2024-02-19 13:48:31 +0100

    ini

--- JQuery/tareasEventosRaton/scripts/11.js
+++ JQuery/tareasEventosRaton/scripts/11.js
@@ -1,5 +1,57 @@
 $(document).ready(function () {
-    $(window).on('load scroll', function (evt) {
-        console.log($(window).scrollTop());
+    //ini
+    $("#menu")
+        .fadeOut(0);
+
+    //scroll
+    $(window).on('load scroll', function () {
+        // $(window).scrollTop(500);
+        if ($(window).scrollTop() >= 240) {
+            $("#volverarriba")
+                .stop(true)
+                .fadeIn(250)
+        } else {
+            $("#volverarriba")
+                .stop(true)
+                .fadeOut(250)
+        }
+        $("#volverarriba").on({
+            "click": function () {
+                $(window)
+                    // .animate()
+                    .scrollTop(500);
+            }
+        })
+    });
+    //menu
+    $("#menu-principal>span").on({
+        "click": function () {
+            // console.log($("#menu").queue()[0]);
+            // if ($("#menu").css("display") == "none" || ($("#menu").css("opacity") != 1 && $("#menu").queue()[0] != "inprogress")) {
+            if ($("#menu").css("display") == "none") {
+                $("#menu")
+                    .stop(true)
+                    .fadeIn(250);
+            } else {
+                $("#menu")
+                    .stop(true)
+                    .fadeOut(250);
+            }
+        }
+    });
+
+    //menu options
+    $("#menu>*").map((i, e) => {
+        $(e).on({
+            "click": function () {
+                if ($(e).children("a").children("span").css("rotate") != "180deg") {
+                    $(e).children("a").children("span")
+                        .animate({ rotate: "180deg" })
+                } else {
+                    $(e).children("a").children("span")
+                        .animate({ rotate: "0deg" })
+                }
+            }
+        })
     });
 });
