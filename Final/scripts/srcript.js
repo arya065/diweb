270138b3de047bfae5307e6d75504a970ba2288e
arya065 <arya065@g.educaand.es>
2024-04-10 21:50:44 +0200

    devide css, ini item page

--- Final/scripts/srcript.js
+++ Final/scripts/srcript.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
+    let page = localStorage.getItem('cur');
+    page = page ?? 'home';
+    console.log(page);
+
+    if (page == "item") {
+        $("header").css("position", "static")
+        $(".header-logo").css("display", "block")
+    } else if (page == "home") {
+        $("header").css("position", "absolute")
+        $(".header-logo").css("display", "none")
+    }
+
     // right menu
     $(".icon-options").on({
         "click": function () {
@@ -22,8 +34,22 @@ $(document).ready(function () {
         }
     })
 
-    // scroll
+    // scroll slider
     $(".slider-container").scroll(function () {
         console.log("slider scroll");
+    })
+
+    // navigation
+    $(".recomended-item").on({
+        "click": function () {
+            localStorage.setItem('cur', 'item');
+            window.location.href = 'item.html';
+        }
+    })
+    $(".header-logo").on({
+        "click": function () {
+            localStorage.setItem('cur', 'home');
+            window.location.href = 'index.html';
+        }
     })
 })
