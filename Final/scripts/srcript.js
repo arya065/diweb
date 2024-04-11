ffb542908e62ecfcd0ee02f8c7577cfcffe0ffff
arya065 <arya065@g.educaand.es>
2024-04-11 14:40:26 +0200

    add info-screen on item page

--- Final/scripts/srcript.js
+++ Final/scripts/srcript.js
@@ -9,6 +9,9 @@ $(document).ready(function () {
     } else if (page == "home") {
         $("header").css("position", "absolute")
         $(".header-logo").css("display", "none")
+    } else if (page == "menu") {
+        $("header").css("position", "static")
+        $(".header-logo").css("display", "block")
     }
 
     // right menu
@@ -23,6 +26,7 @@ $(document).ready(function () {
         "click": function () {
             $(".shadow-mask").css("display", "none")
             $(".right-menu").css("display", "none")
+            $(".info-screen").css("display", "none")
             $("body").css("overflow", "scroll")
         }
     })
@@ -38,18 +42,39 @@ $(document).ready(function () {
     $(".slider-container").scroll(function () {
         console.log("slider scroll");
     })
+    // info-screen
+    $(".item-controls-btn").on({
+        "click": function () {
+            $(".shadow-mask").css("display", "block")
+            $(".info-screen").css("display", "flex")
+            $("body").css("overflow", "hidden")
+        }
+    })
+    $(".info-screen-btn").on({
+        "click": function () {
+            $(".shadow-mask").css("display", "none")
+            $(".info-screen").css("display", "none")
+            $("body").css("overflow", "scroll")
+        }
+    })
 
     // navigation
+    $(".header-logo").on({
+        "click": function () {
+            localStorage.setItem('cur', 'home');
+            window.location.href = 'index.html';
+        }
+    })
     $(".recomended-item").on({
         "click": function () {
             localStorage.setItem('cur', 'item');
             window.location.href = 'item.html';
         }
     })
-    $(".header-logo").on({
+    $(".right-menu-options>p:nth-child(1)").on({
         "click": function () {
-            localStorage.setItem('cur', 'home');
-            window.location.href = 'index.html';
+            localStorage.setItem('cur', 'menu');
+            window.location.href = 'menu.html';
         }
     })
 })
